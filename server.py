60eb2b21f8558b5d0f987513eebdadfe2d5fc729
Brian Chen <enderchen004@outlook.com>
2024-04-03 11:04:11 +0100

    Pretty much completed, except the time still needs adjusting

--- server.py
+++ server.py
@@ -30,7 +30,7 @@ pipe = pipeline(
 
 model, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
                               model='silero_vad',
-                              force_reload=True,
+                              force_reload=False,
                               onnx=False)
 (get_speech_timestamps,
  save_audio,
@@ -55,6 +55,7 @@ frames5 = []
 frames10 = []
 all_frames = []
 all_speech_probs = []
+tot_len = 0
 recording = False
 BOUNDARY = 0.3  # needs adjusting per device/scenario
 
@@ -62,6 +63,7 @@ final_result = ""
 calc_result_running = False
 fallback = False
 
+my_thread = None
 
 def calc_result(counts):
     global calc_result_running, final_result
@@ -78,10 +80,10 @@ def calc_result(counts):
 
 def audio_recording():
     global frames5, frames10, all_frames, all_speech_probs, fallback
-    audio = pyaudio.PyAudio()
-    stream = audio.open(format=FORMAT, channels=CHANNELS,
-                        rate=RATE, input=True,
-                        frames_per_buffer=CHUNK)
+    #audio = pyaudio.PyAudio()
+    #stream = audio.open(format=FORMAT, channels=CHANNELS,
+    #                    rate=RATE, input=True,
+    #                    frames_per_buffer=CHUNK)
     print("Recording started...")
     # all_speech_probs = []
     tot_len = 0
@@ -89,6 +91,7 @@ def audio_recording():
         if not recording:
             continue
         data = stream.read(CHUNK)
+        #print("data length: ", len(data))
         all_frames.append(data)
         # frames5.append(data) #frames5: 6-15, 16-25, 26-35, 36-45, ...
         frames10.append(data)  # frames10: 1-10, 11-20, 21-30, 31-40, ...
@@ -188,13 +191,117 @@ def index():
 
 @socketio.on('start_recording')
 def start_recording():
-    global frames5, frames10, all_frames, recording
+    print("Recording Started")
+    global frames5, frames10, all_frames, recording, tot_len
     frames5 = []  # Clear existing frames
     frames10 = []
     all_frames = []
     recording = True
-    threading.Thread(target=audio_recording).start()
+    tot_len = 0
+    #threading.Thread(target=audio_recording).start()
 
+@socketio.on('audio_data')
+def handle_audio_data(data):
+    global frames5, frames10, all_frames, all_speech_probs, fallback, tot_len, my_thread
+    #print('Received audio data length:', len(data))
+    socketio.emit('audio_received')
+    all_frames.append(data)
+    # frames5.append(data) #frames5: 6-15, 16-25, 26-35, 36-45, ...
+    frames10.append(data)  # frames10: 1-10, 11-20, 21-30, 31-40, ...
+
+    tot_len += 1
+    
+    local_len = tot_len
+    
+    if local_len >= 6:
+        frames5.append(data)
+
+    if local_len % 10 == 0 and not fallback:
+        filename = "test_" + str(local_len) + ".wav"
+        wf = wave.open(filename, 'wb')
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(pyaudio.PyAudio().get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(all_frames))
+        wf.close()
+        if calc_result_running and my_thread != None:
+            if my_thread.is_alive():
+                fallback = True
+                print("Falling back to processing after finish.")
+            # print("Killing thread: " + str(my_thread.ident))
+            # ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(my_thread.ident), ctypes.py_object(SystemExit))
+        if not fallback:
+            my_thread = threading.Thread(
+                target=calc_result, args=(local_len,))
+            # print("Running new thread")
+            my_thread.start()
+
+    # 200-250ms intervals fed into vad is usually good
+    if len(frames5) == 10:
+        frames5_data = b''.join(frames5)
+        numbers = [int.from_bytes(frames5_data[i:i+2], byteorder='little', signed=False)
+                    for i in range(0, len(frames5_data), 2)]
+
+        # Normalize integers to range between 0 and 2
+        max_val = max(numbers)
+        min_val = min(numbers)
+        normalized_numbers = [(num - min_val) * 2 /
+                                (max_val - min_val) for num in numbers]
+
+        wav = torch.tensor(normalized_numbers, dtype=torch.float32)
+
+        window_size_samples = 1536
+        speech_probs = []
+        for i in range(0, len(wav), window_size_samples):
+            chunk = wav[i: i + window_size_samples]
+            if len(chunk) < window_size_samples:
+                break
+            speech_prob = model(chunk, 16000).item()
+            speech_probs.append(speech_prob)
+        vad_iterator.reset_states()
+        all_speech_probs += speech_probs[:10]
+
+        print(max(speech_probs[:10]))
+
+        if max(speech_probs[:10]) <= BOUNDARY:
+            socketio.emit('stop_recording')
+            print("Stop recording called")
+            #stop_recording()
+
+        frames5 = []
+
+    if len(frames10) == 10:
+        frames10_data = b''.join(frames10)
+        numbers = [int.from_bytes(frames10_data[i:i+2], byteorder='little', signed=False)
+                    for i in range(0, len(frames10_data), 2)]
+
+        # Normalize integers to range between 0 and 2
+        max_val = max(numbers)
+        min_val = min(numbers)
+        normalized_numbers = [(num - min_val) * 2 /
+                                (max_val - min_val) for num in numbers]
+
+        wav = torch.tensor(normalized_numbers, dtype=torch.float32)
+
+        window_size_samples = 1536
+        speech_probs = []
+        for i in range(0, len(wav), window_size_samples):
+            chunk = wav[i: i + window_size_samples]
+            if len(chunk) < window_size_samples:
+                break
+            speech_prob = model(chunk, 16000).item()
+            speech_probs.append(speech_prob)
+        vad_iterator.reset_states()
+        all_speech_probs += speech_probs[:10]
+
+        print(max(speech_probs[:10]))
+
+        if max(speech_probs[:10]) <= BOUNDARY:
+            socketio.emit('stop_recording')
+            print("Stop recording called")
+            #stop_recording()
+
+        frames10 = []
 
 @socketio.on('stop_recording')
 def stop_recording():
@@ -206,6 +313,7 @@ def stop_recording():
         while calc_result_running:
             continue
         print(final_result)
+        socketio.emit('transcription_available', final_result)
 
     else:
         filename = 'recording_all.wav'
@@ -217,7 +325,7 @@ def stop_recording():
         wf.close()
         result = pipe("recording_all.wav")
         print(result['text'])
-
+        socketio.emit('transcription_available', result['text'])
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
